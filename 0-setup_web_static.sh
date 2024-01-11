5ef307e18f002848e30588035770efc0cbf6e871
Medidali <m.mohamedidali.com>
2024-01-11 14:45:44 -0800

    all task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,33 @@
 #!/usr/bin/env bash
-#sets up the web server for the deployment of web_static
+# Sets up a web server for deployment of web_static.
 
-apt update
-apt install nginx -y
+apt-get update
+apt-get install -y nginx
 
-mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echo "This shit is working" > /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu:ubuntu /data/
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
 printf %s "server {
-    listen 80;
-    listen [::]:80;
+    listen 80 default_server;
+    listen [::]:80 default_server;
     add_header X-Served-By $HOSTNAME;
-    root /var/www/html;
-    index index.html;
+    root   /var/www/html;
+    index  index.html index.htm;
+
     location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html;
+	alias /data/web_static/current;
+	index index.html index.htm;
     }
+
     location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+	return 301 http://cuberule.com/;
     }
+
     error_page 404 /404.html;
     location /404 {
       root /var/www/html;
